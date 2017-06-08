9afd8612ea8d23896d2a1c05890bd7e41665a9fc
github username <alexsambvani@gmail.com>
2017-06-08 14:25:11 -0400

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,31 @@
-# code here!
+class School
+
+  def initialize (school_name)
+    @school_name = school_name
+    @roster = {}
+  end
+
+  def add_student(student_name, grade)
+    if roster.keys.include?(grade)
+      roster[grade] << student_name
+    else
+      roster[grade] = []
+      roster[grade] << student_name
+    end
+  end
+
+  def roster
+    @roster
+  end
+
+  def grade(grade)
+    roster[grade]
+  end
+
+  def sort
+    @roster.each_with_object({}) do |key_values, sorted_roster|
+      sorted_roster[key_values[0]] = key_values[1].sort
+    end
+  end
+
+end
